5d3cea4252c20d185fd30219122e3f00302791d9
shah-here <shahrinakhan6@gmail.com>
2023-05-27 12:55:00 +0600

    m

--- src/Components/CopyText.js
+++ src/Components/CopyText.js
@@ -1,36 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import CopyToClipboard from "react-copy-to-clipboard";
 import { FaCopy } from "react-icons/fa";
-import { Tooltip } from "react-bootstrap";
 
 function CopyText({ text }) {
-  const [copied, setCopied] = useState(false);
-
-  const handleCopy = () => {
-    setCopied(true);
-    setTimeout(() => {
-      setCopied(false);
-    }, 2000);
-  };
-
   return (
     <div
       className="border border-3 rounded mb-3 mt-1 d-flex justify-content-between align-items-center container"
       style={{ width: "445px", height: "50px", backgroundColor: "#F8F0E3" }}
     >
       <div>{text}</div>
-      <CopyToClipboard text={text} onCopy={handleCopy}>
-        <FaCopy
-          style={{ cursor: "pointer" }}
-          onMouseOver={(e) => (e.currentTarget.style.color = "green")}
-          onMouseOut={(e) => (e.currentTarget.style.color = "")}
-        />
+
+      <CopyToClipboard text={text}>
+        <button className="btn">
+          <FaCopy
+            style={{ cursor: "pointer" }}
+            onMouseOver={(e) => (e.currentTarget.style.color = "green")}
+            onMouseOut={(e) => (e.currentTarget.style.color = "")}
+          />
+        </button>
       </CopyToClipboard>
-      {copied && (
-        <Tooltip placement="top" show>
-          Link copied
-        </Tooltip>
-      )}
     </div>
   );
 }
